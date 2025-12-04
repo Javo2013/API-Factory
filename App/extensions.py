cbac52638f164b60e40a4a01cc3f24edb99e04e6
Javian Williams <javian.williams29@gmail.com>
2025-12-03 20:48:23 -0500

    add additional flask imports

--- App/extensions.py
+++ App/extensions.py
@@ -1,7 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_caching import Cache
 
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
+
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["100 per minute"]   
+)
+
+cache = Cache()
